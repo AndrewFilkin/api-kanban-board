8f2bb71f7fc90e07638427a51efe7d5eb5cfcb9b
AndrewFilkin <filkin.andrew.s@gmail.com>
2022-08-21 23:24:23 +0300

    add 4 migrate

--- app/Models/InProgressDashboardModel.php
+++ app/Models/InProgressDashboardModel.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class inProgress extends Model
+class InProgressDashboardModel extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['title'];
 }
